1ca5377567d57349993a8b269b19ea4ebd735edc
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2022-09-06 18:49:45 -0300

    Implementação dos testes do Model Medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -6,4 +6,15 @@ class MedicoTest < ActiveSupport::TestCase
                         especialidade: 'Reumatologista', crm: '123456'
     assert medico.save
   end
+
+  test "nao deveria atualizar um medico com CPF invalido" do
+    medico = Medico.new(nome:'Antonio Silva', cpf:'15151805061', email:'antoniosilva@gmail.com', especialidade:'cirugião', crm:'12345')
+    assert medico.save
+    assert_not medico.update(nome:'Antonio Silva', cpf:'12345678900', email:'antoniosilva@gmail.com', especialidade:'cirugião', crm:'12345')
+  end
+
+  test "nao deveria criar um medico com email invalido" do
+    medico = Medico.new(nome:'Antonio Silva', cpf:'15151805061', email:'antoniosilva', especialidade:'cirugião', crm:'12345')
+    assert_not medico.save
+  end
 end
