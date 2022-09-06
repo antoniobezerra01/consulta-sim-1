e1091e758ee2f246a2cec9ec7f04dcf41624169e
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2022-09-06 19:33:07 -0300

    Implementação dos testes do Model Paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'nao deveria salvar um paciente com atributos nulos' do
+    paciente = Paciente.new
+    assert_not paciente.save
+  end
+
+  test 'deveria salvar um paciente com atributos preenchidos corretamente' do
+    paciente = Paciente.new(nome: 'Antonio Bezerra', data_nascimento: '18-03-2002', cpf: '61081490004', email: 'antonio_@gmail.com')
+    paciente.endereco = Endereco.new(cep: '12345-678', cidade: 'Garanhuns', bairro: 'Magano', logradouro: 'rua x', complemento: 'casa x')
+    assert paciente.save
+  end
 end
